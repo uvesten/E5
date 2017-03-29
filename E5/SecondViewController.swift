5714fd192c29bb619a85267e128d03aba92cc411
Petter Uvesten <uvesten@everichon.com>
2017-03-29 10:41:18 +0200

    Initial Commit

---
+++ E5/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  E5
+//
+//  Created by Petter Uvesten on 2017-03-29.
+//  Copyright © 2017 Everichon AB. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ E5Tests/E5Tests.swift
@@ -0,0 +1,36 @@
+//
+//  E5Tests.swift
+//  E5Tests
+//
+//  Created by Petter Uvesten on 2017-03-29.
+//  Copyright © 2017 Everichon AB. All rights reserved.
+//
+
+import XCTest
+@testable import E5
+
+class E5Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
